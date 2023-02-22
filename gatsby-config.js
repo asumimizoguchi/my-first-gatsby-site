c7e72af0cb84e5dba10c3ff1ec624657f8487eb9
溝口明寿美 <asumi@mizogucoMacBook.lan>
2023-02-22 15:00:36 -0500

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `What would you like to call your site?✔ · My First Gatsby Site`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-google-gtag"]
+};
